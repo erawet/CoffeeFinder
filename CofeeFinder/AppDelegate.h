e87e08372a6f46d12c21e2a0f8834b7740ab8b0b
Don Wettasinghe <donwettasinghe@Dons-MacBook-Pro.local>
2015-01-03 15:21:06 -0500

    Initial Commit

---
+++ CofeeFinder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CofeeFinder
+//
+//  Created by Don Wettasinghe on 1/3/15.
+//  Copyright (c) 2015 Don Wettasinghe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
